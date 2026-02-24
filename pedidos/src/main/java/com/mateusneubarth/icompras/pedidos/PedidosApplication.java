3f8f034a6f45271c06a9f21d5437e325af255779
mneubarth <mneubarth@grupopanvel.com.br>
2026-02-24 10:09:45 -0300

    feat: integrate Kafka messaging with configuration and producer setup

--- pedidos/src/main/java/com/mateusneubarth/icompras/pedidos/PedidosApplication.java
+++ pedidos/src/main/java/com/mateusneubarth/icompras/pedidos/PedidosApplication.java
@@ -1,10 +1,22 @@
 package com.mateusneubarth.icompras.pedidos;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class PedidosApplication {
+
+	// @Bean
+	public CommandLineRunner commandLineRunner(KafkaTemplate<String, String> template) {
+		return args -> {
+			Thread.sleep(5000);
+			template.send("icompras.pedidos-pagos", "dados", "{ json com os dados do pedido }");
+			System.out.println("Mensagem enviada!");
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
